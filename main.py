79054d847489e54acaf9dbd4ec869848e2139d74
unknown <you@example.com>
2025-03-24 22:17:54 +0100

    country and city finding by ip basic

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from base import makingDatabases,UserById,addUserAddress
 from flask import Flask, render_template, url_for, redirect, request
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from simpleFunctions import singInPersonalInfoPost, logInPost
+import requests
 
 makingDatabases()
 
@@ -16,11 +17,22 @@ login_manager.login_view = "logIn"
 def load_user(user_id):
     return UserById(int(user_id))
 
+def get_location(ip):
+    url = f"http://ip-api.com/json/{ip}"
+    response = requests.get(url)
+    data = response.json()
+    country = data.get("country", "Nepoznata država")
+    city = data.get("city", "Nepoznat grad")
+    return country, city
+
 @app.route("/")
 def homeUser():
     if current_user.is_authenticated:
         return current_user.userName
-    return "LogOut"
+    #i get ip like this when i test code on local machine but when server start going live i need to use user_ip = request.remote_addr
+    user_ip = requests.get("https://api64.ipify.org?format=json").json()["ip"]
+    country, city = get_location(user_ip)
+    return f"Vaša IP adresa: {user_ip}, Država: {country}, Grad: {city}"
 
 @app.route("/signInPersonalInfo",methods= ["GET","POST"] )
 def signInUserPersonalInfo():
